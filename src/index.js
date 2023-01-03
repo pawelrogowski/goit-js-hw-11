163a4763013f36200f3b2855d86ceb35b2bf35d4
Pawel Rogowski <pawel.rogowski.praca@gmail.com>
2023-01-03 15:26:20 +0100

    modified:   src/index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// 1. Set up search form in HTML file and add event listener for form submission
+// 2. Sign up for Pixabay API and get unique access key
+// 3. Read Pixabay API documentation and understand query string parameters and pagination parameters
+// 4. Install and import Axios library in JavaScript file
+// 5. Write function to make HTTP request to Pixabay API using Axios, with specified query string parameters (key, q, image_type, orientation, safesearch) and pagination parameters (page, per_page)
+// 6. Install and import Notiflix library in JavaScript file
+// 7. Write function to show notification using Notiflix library if API returns empty array
+// 8. Write function to parse response from API and extract webformatURL, largeImageURL, tags, likes, views, comments, and downloads properties for each image object in array
+// 9. Write function to render image card markup for each image object into the gallery element in HTML, using the extracted properties
+// 10. Show "Load more" button in interface and add event listener for click event
+// 11. Write function to make another HTTP request to API and render additional image cards when "Load more" button is clicked, incrementing the page parameter each time
+// 12. Write function to hide "Load more" button and show notification using Notiflix if total number of images reached (totalHits property)
+// 13. Write function to show notification using Notiflix with text "Hooray! We found totalHits images." after each search
+// 14. Install and import SimpleLightbox library in JavaScript file
+// 15. Write function to set up SimpleLightbox to display large image when image card is clicked
+// 16. Ensure that during live page visits, there are no errors or warnings generated in the console
+// 17. Use async/await syntax for HTTP requests.
