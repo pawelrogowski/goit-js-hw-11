ea83abafbfd2b46eade0363ba1a95defbd47ae8e
Pawel Rogowski <pawel.rogowski.praca@gmail.com>
2023-01-04 18:07:17 +0100

    removed .env, api keys from deployment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-const apiKey = process.env.PIXABAY_API_KEY;
-console.log(apiKey);
-console.log('as');
+const apiKey = '32579471-afdc8e0303a1983f0362481fc';
+
 fetch(`https://pixabay.com/api/?key=${apiKey}&q=yellow+flowers&image_type=photo`).then(data => console.log(data));
