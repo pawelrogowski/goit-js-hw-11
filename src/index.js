f83f371321614549736edde84aa35a84eed780e3
Pawel Rogowski <pawel.rogowski.praca@gmail.com>
2023-01-04 18:02:37 +0100

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ require('dotenv').config();
 const apiKey = process.env.PIXABAY_API_KEY;
 console.log(apiKey);
 console.log('as');
+fetch(`https://pixabay.com/api/?key=${apiKey}&q=yellow+flowers&image_type=photo`).then(data => console.log(data));
