ff2704987c87517ddf744a0f0d04adae9ca2ea56
Pawel Rogowski <pawel.rogowski.praca@gmail.com>
2023-01-05 00:13:34 +0100

    added basic functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-const apiKey = '32579471-afdc8e0303a1983f0362481fc';
+import axios from 'axios';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 
-fetch(`https://pixabay.com/api/?key=${apiKey}&q=yellow+flowers&image_type=photo`).then(data => console.log(data));
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('.search-form__input');
+const galleryElement = document.querySelector('.gallery');
+
+async function fetchImage(query, options) {
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: '32579471-afdc8e0303a1983f0362481fc',
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        ...options,
+      },
+    });
+    return response.data.hits;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+searchForm.addEventListener('submit', async function (event) {
+  event.preventDefault();
+  const query = searchInput.value;
+  const imageData = await fetchImage(query, {});
+  galleryElement.innerHTML = '';
+  let imageHTML = '';
+  imageData.forEach(image => {
+    imageHTML += `<a class="gallery__item" href="${image.largeImageURL}"><img class="gallery__image" src="${image.webformatURL}"></a>`;
+  });
+  galleryElement.innerHTML = imageHTML;
+  const lightbox = new SimpleLightbox('.gallery .gallery__item', {
+    captionPosition: 250,
+    disableScroll: false,
+  });
+  lightbox.refresh();
+});
