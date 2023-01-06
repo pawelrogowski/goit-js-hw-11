85b43694b9db211168fa3275657e68000588426f
Pawel Rogowski <pawel.rogowski.praca@gmail.com>
2023-01-07 00:01:46 +0100

    modified:   package-lock.json 	modified:   src/css/main.css.map 	modified:   src/css/main.min.css 	modified:   src/css/main.min.css.map 	modified:   src/index.js 	modified:   src/scss/main.scss

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 import axios from 'axios';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import SimpleLightbox from 'simplelightbox';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { debounce } from 'lodash';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const searchForm = document.querySelector('#search-form');
+const searchForm = document.querySelector('.search-form');
 const searchInput = document.querySelector('.search-form__input');
 const galleryElement = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
 const pageBottom = document.querySelector('.page-bottom');
 
-loadMoreButton.style.display = 'none';
-let firstSearchCompleted = false;
 let lastQuery = null;
 let page = 1;
-
-function isNewQuery() {
-  if (lastQuery !== searchInput.value) {
-    return true;
-  }
-  return false;
-}
+let totalPages = 0;
 
 async function fetchImage(query, options, page) {
   try {
@@ -36,18 +29,20 @@ async function fetchImage(query, options, page) {
         ...options,
       },
     });
-
-    // if (!firstSearchCompleted) {
-    if (response.data.totalHits > 0 && isNewQuery()) {
-      Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
-    } else if (isNewQuery()) {
-      Notify.failure(`Sorry no images for "${searchInput.value}" found`);
+    totalPages = Math.ceil(response.data.totalHits / 40);
+    if (response.data.totalHits === 0) {
+      Notify.failure(`Sorry no images for "${searchInput.value}" found`, {
+        position: 'right-top',
+      });
+    } else if (page === 1) {
+      Notify.success(`Hooray! We found ${response.data.totalHits} images.`, {
+        position: 'right-top',
+      });
     }
-    // }
+
     return response.data.hits;
   } catch (error) {
-    console.error(error);
-    Notify.failure('An error occurred while fetching images');
+    console.log(error);
   }
 }
 
@@ -63,42 +58,55 @@ function updateGallery(imageData) {
         <div class="gallery__caption">Views: ${image.views}</div>
         <div class="gallery__caption">Comments: ${image.comments}</div>
         <div class="gallery__caption">Downloads: ${image.downloads}</div>
-      </figcaption>
+  </figcaption>
     </figure>
   </a>`;
   });
   galleryElement.innerHTML += imageHTML;
   lightbox.refresh();
+  if (page === 1 && totalPages !== 0) {
+    loadMoreButton.style.display = 'block';
+  } else {
+    loadMoreButton.style.display = 'none';
+  }
 }
 
 const lightbox = new SimpleLightbox('.gallery .gallery__item');
 
-searchForm.addEventListener('submit', async function (event) {
-  event.preventDefault();
-  galleryElement.innerHTML = '';
-  page = 1;
-  const imageData = await fetchImage(searchInput.value, {}, page);
-  updateGallery(imageData);
-  firstSearchCompleted = true;
-  lastQuery = searchInput.value;
-  if (galleryElement.innerHTML !== '') {
-    loadMoreButton.style.display = 'block';
+const debouncedSearch = debounce(async function () {
+  const query = searchInput.value;
+  if (query === lastQuery) {
+    return;
+  } else {
+    galleryElement.innerHTML = '';
   }
+  lastQuery = query;
+  page = 1;
+  const imageData = await fetchImage(query, {}, page);
+  updateGallery(imageData);
+}, 300);
+
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  debouncedSearch();
 });
 
 loadMoreButton.addEventListener('click', async function () {
-  page++;
-  const imageData = await fetchImage(searchInput.value, {}, page);
+  page += 1;
+  const imageData = await fetchImage(lastQuery, {}, page);
   updateGallery(imageData);
-  observer.observe(pageBottom);
-  loadMoreButton.style.display = 'none';
+  pageBottomObserver.observe(pageBottom);
 });
 
-const observer = new IntersectionObserver(async (entries, observer) => {
-  if (entries[0].isIntersecting) {
-    page++;
-    console.log('Asdasdasd');
-    const imageData = await fetchImage(searchInput.value, {}, page);
-    updateGallery(imageData);
+const pageBottomObserver = new IntersectionObserver(async function (entries, observer) {
+  if (entries[0].isIntersecting === false) return;
+  if (page >= totalPages) {
+    Notify.info("You've reached the end of search results", {
+      position: 'right-bottom',
+    });
+    return;
   }
+  page += 1;
+  const imageData = await fetchImage(lastQuery, {}, page);
+  updateGallery(imageData);
 });
